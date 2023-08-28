fae49ad5b7153c259c2a529e747e3c89548fd3b5
Yonatan Ariel <yonatanriel@gmail.com>
2023-08-28 20:22:58 +0300

    adding profilePhoto component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,26 +1,39 @@
 import HeaderNav from "../HeaderNav";
+import ProfilePhoto from "../ProfilePhoto";
 import styles from "./style.module.css";
 function Layout() {
   return (
     <div className={styles.backgroundImg}>
-      <HeaderNav />
       <div
         style={{
-          margin: "20px auto 0 auto",
-          backdropFilter: "blur(10px)",
-          border: "2px white solid",
-          width: "min(400px, 95vw)",
-          height: " 440px",
-          borderRadius: "11px",
-          color: "white",
-          padding: "40px",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0,0,0,0.1)",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
         }}
       >
-        <h1>Lorem ipsum dolor sit amet </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-          porro commodi rem explicabo velit provident.
-        </p>
+        <HeaderNav />
+        <div
+          style={{
+            margin: "20px auto 0 auto",
+            backdropFilter: "blur(10px)",
+            border: "2px white solid",
+            width: "min(400px, 95vw)",
+            height: " 440px",
+            borderRadius: "11px",
+            color: "white",
+            padding: "40px",
+          }}
+        >
+          <h1>Lorem ipsum dolor sit amet </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            porro commodi rem explicabo velit provident.
+          </p>
+        </div>
+        <ProfilePhoto />
       </div>
     </div>
   );
---
+++ src/components/ProfilePhoto/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./style.module.css";
+
+import React from "react";
+
+function ProfilePhoto() {
+  return (
+    <div className={styles.container}>
+      {/* <h1>Animating border radius</h1> */}
+    </div>
+  );
+}
+
+export default ProfilePhoto;
