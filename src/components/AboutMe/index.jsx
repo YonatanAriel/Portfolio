96b719a6907a5ad29709552d7a61046a5bf9f6ee
Yonatan Ariel <yonatanriel@gmail.com>
2023-09-02 00:17:38 +0300

    improving typing animation

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -9,26 +9,26 @@ function AboutMe() {
   const textLines =
     screenWidth > 600
       ? [
-          "Coding is my true love.",
-          "Driven by this passion, I'm highly motivated",
-          " to continuously expand my knowledge,",
-          "enabling me to adapt quickly to new challenges",
-          " and technologies.",
-          "I genuinely enjoy working with people and ",
-          "possess the capability to lead when necessary.",
+          { txt: "Coding is my true love.", time: 3000 },
+          { txt: "Driven by this passion, I'm highly motivated", time: 2000 },
+          { txt: " to continuously expand my knowledge,", time: 2000 },
+          { txt: "enabling me to adapt quickly to new challenges", time: 2400 },
+          { txt: " and technologies.", time: 2000 },
+          { txt: "I genuinely enjoy working with people and ", time: 2000 },
+          { txt: "possess the capability to lead when necessary.", time: 2000 },
         ]
       : [
-          "Coding is my true love.",
-          "Driven by this passion,  ",
-          "I'm highly motivated to",
-          "continuously expand my ",
-          "knowledge, enabling me to",
-          "adapt quickly to new",
-          "challenges and technologies.",
-          "I genuinely enjoy working",
-          "with people and possess",
-          "the capability to lead",
-          "when necessary.",
+          { txt: "Coding is my true love.", time: 2800 },
+          { txt: "Driven by this passion,  ", time: 3000 },
+          { txt: "I'm highly motivated to", time: 2200 },
+          { txt: "continuously expand my ", time: 2200 },
+          { txt: "knowledge, enabling me to", time: 2200 },
+          { txt: "adapt quickly to new", time: 2200 },
+          { txt: "challenges and technologies.", time: 2000 },
+          { txt: "I genuinely enjoy working", time: 3500 },
+          { txt: "with people and possess", time: 2200 },
+          { txt: "the capability to lead", time: 2200 },
+          { txt: "when necessary.", time: 2200 },
         ];
   const facts = [
     {
@@ -65,7 +65,7 @@ function AboutMe() {
     if (animationIndex < textLines.length) {
       const timer = setTimeout(() => {
         setAnimationIndex(animationIndex + 1);
-      }, 3000);
+      }, textLines[animationIndex].time);
 
       return () => clearTimeout(timer);
     }
@@ -75,17 +75,14 @@ function AboutMe() {
     <div id="about me" className={`${styles.container} d-flex`}>
       <div>
         <h1>
-          <span>A bit</span> <span> about</span>
+          <span className={styles.aBit}>A bit</span> <span> about</span>
           <span> me..</span>
         </h1>
-        <p>
-          {textLines.slice(0, animationIndex).map((line, index) => (
-            <span key={index} style={{ display: "inline-block" }}>
-              {line}
-              <br />
-            </span>
+        <div className={styles.txtContainer}>
+          {textLines.slice(0, animationIndex).map((line, i) => (
+            <div key={i}>{line.txt}</div>
           ))}
-        </p>
+        </div>
       </div>
       <ul className={styles.facts}>
         {facts.map((fact, i) => (
