de2612e337f959444dcc6dca847377c008dcd950
j s <grnwtmry@gmail.com>
2023-09-11 01:45:27 +0300

    adding components - SkillsContainer + SkillText

---
+++ src/components/SkillText/index.jsx
@@ -0,0 +1,8 @@
+import styles from "./style.module.css";
+import React from "react";
+
+function SkillText() {
+  return <div></div>;
+}
+
+export default SkillText;
---
+++ src/components/SkillsContainer/index.jsx
@@ -0,0 +1,8 @@
+import styles from "./style.module.css";
+import React from "react";
+
+function SkillsContainer() {
+  return <div></div>;
+}
+
+export default SkillsContainer;
